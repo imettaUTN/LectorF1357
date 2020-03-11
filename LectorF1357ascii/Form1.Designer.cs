3bf7b41c450ce3102a57b00c6f8ffdaac8f36e88
Ivan Metta <ivanmetta@gmail.com>
2020-03-11 16:09:42 -0300

    Se sube version del lector

---
+++ LectorF1357ascii/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace LectorF1357ascii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOrigen = new System.Windows.Forms.TextBox();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(591, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Este programa generará un nuevo archivo separado segun diseño con cada campo de c" +
+    "ada registro x legajo Para su testeo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(307, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ruta origen archivo F1357 ( se sugiere dentro del directorio C:/)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(312, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ruta destino archivo F1357 ( se sugiere dentro del directorio C:/)";
+            // 
+            // txtOrigen
+            // 
+            this.txtOrigen.Location = new System.Drawing.Point(58, 37);
+            this.txtOrigen.Name = "txtOrigen";
+            this.txtOrigen.Size = new System.Drawing.Size(412, 20);
+            this.txtOrigen.TabIndex = 3;
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.Location = new System.Drawing.Point(58, 96);
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.Size = new System.Drawing.Size(412, 20);
+            this.txtDestino.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 219);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDestino);
+            this.Controls.Add(this.txtOrigen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOrigen;
+        private System.Windows.Forms.TextBox txtDestino;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ LectorF1357ascii/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LectorF1357ascii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] TiposRegistros = { "01", "02", "03", "04", "05", "06" };
+
+            using (System.IO.StreamWriter fileWrite = new StreamWriter(this.txtDestino.Text))
+            {
+                using (StreamReader fielRead = new StreamReader(this.txtOrigen.Text))
+                {
+                    String line;
+                    while ((line = fielRead.ReadLine()) != null)
+                    {
+
+                        switch (line.Substring(0, 2))
+                        {
+                            case "01":
+                                fileWrite.WriteLine("Valor Campo:   Tamaño Campo:     Descripcion campo:");
+                                fileWrite.WriteLine(line.Substring(0, 2).PadRight(15) + "  " + line.Substring(0, 2).Length.ToString().PadRight(13) + "      Tipo Registro");
+                                fileWrite.WriteLine(line.Substring(2, 11).PadRight(15) + "  " + line.Substring(2, 11).Length.ToString().PadRight(13)+  "      Cuit agente retencion");
+                                fileWrite.WriteLine(line.Substring(13, 6).PadRight(15) + "  " + line.Substring(13, 6).Length.ToString().PadRight(13) + "      Periodo informado");
+                                fileWrite.WriteLine(line.Substring(19, 2).PadRight(15) + "  " + line.Substring(19, 2).Length.ToString().PadRight(13) + "     Secuencia");
+                                fileWrite.WriteLine(line.Substring(21, 4).PadRight(15) + "  " + line.Substring(21, 4).Length.ToString().PadRight(13) + "      Codigo de impuesto");
+                                fileWrite.WriteLine(line.Substring(25, 3).PadRight(15) + "  " + line.Substring(25, 3).Length.ToString().PadRight(13) + "      Codigo de concepto");
+                                fileWrite.WriteLine(line.Substring(28, 4).PadRight(15) + "  " + line.Substring(28, 4).Length.ToString().PadRight(13) + "      numero de formulario");
+                                fileWrite.WriteLine(line.Substring(32, 1).PadRight(15) + "  " + line.Substring(32, 1).Length.ToString().PadRight(13) + "      tipo de presentacion");
+                                fileWrite.WriteLine(line.Substring(33, 5).PadRight(15) + "  " + line.Substring(33, 5).Length.ToString().PadRight(13) + "      version del sistema" + "\n");
+                                fileWrite.WriteLine("");
+                                break;
+                            case "02":
+                                fileWrite.WriteLine("Valor Campo:   Tamaño Campo:     Descripcion campo:");
+                                fileWrite.WriteLine(line.Substring(0, 2).PadRight(15).PadRight(15) +"   " + line.Substring(0, 2).Length.ToString().PadRight(13) + "      Tipo Registro");
+                                fileWrite.WriteLine(line.Substring(2, 11).PadRight(15) +"   " + line.Substring(2, 11).Length.ToString().PadRight(13) + "      CUIL");
+                                fileWrite.WriteLine(line.Substring(13, 8).PadRight(15) +"   " + line.Substring(13, 8).Length.ToString().PadRight(13) + "      Periodo tbj desde");
+                                fileWrite.WriteLine(line.Substring(21, 8).PadRight(15) +"   " + line.Substring(21, 8).Length.ToString().PadRight(13) + "      Periodo trb hasta");
+                                fileWrite.WriteLine(line.Substring(29, 2).PadRight(15) +"   " + line.Substring(29, 2).Length.ToString().PadRight(13) + "      Meses");
+                                fileWrite.WriteLine(line.Substring(31, 1).PadRight(15) +"   " + line.Substring(31, 1).Length.ToString().PadRight(13) + "      Beneficio");
+                                fileWrite.WriteLine(line.Substring(32, 1).PadRight(15) +"   " + line.Substring(32, 1).Length.ToString().PadRight(13) + "      ¿Trabajo larga distancia?" + "\n");
+                                fileWrite.WriteLine("");
+                                break;
+                            case "03":
+                                fileWrite.WriteLine("Valor Campo:   Tamaño Campo:     Descripcion campo:");
+                                fileWrite.WriteLine(line.Substring(0, 2).PadRight(15) +"   " + line.Substring(0, 2).Length.ToString().PadRight(13) + "      Tipo Registro");
+                                fileWrite.WriteLine(line.Substring(2, 11).PadRight(15) +"   " + line.Substring(2, 11).Length.ToString().PadRight(13) + "      CUIL");
+                                fileWrite.WriteLine(line.Substring(13, 15).PadRight(15) +"   " + line.Substring(13, 15).Length.ToString().PadRight(13) + "      Remuneracion bruta");
+                                fileWrite.WriteLine(line.Substring(28, 15).PadRight(15) +"   " + line.Substring(28, 15).Length.ToString().PadRight(13) + "      Remuneracion no hab");
+                                fileWrite.WriteLine(line.Substring(43, 15).PadRight(15) +"   " + line.Substring(43, 15).Length.ToString().PadRight(13) + "      SAC 1 cuota");
+                                fileWrite.WriteLine(line.Substring(58, 15).PadRight(15) +"   " + line.Substring(58, 15).Length.ToString().PadRight(13) + "      SAC 2 cuota");
+                                fileWrite.WriteLine(line.Substring(73, 15).PadRight(15) +"   " + line.Substring(73, 15).Length.ToString().PadRight(13) + "      Hs extras gravadas");
+                                fileWrite.WriteLine(line.Substring(88, 15).PadRight(15) +"   " + line.Substring(88, 15).Length.ToString().PadRight(13) + "      Mov y viaticos rem grv");
+                                fileWrite.WriteLine(line.Substring(103, 15).PadRight(15) +"   " + line.Substring(103, 15).Length.ToString().PadRight(13) + "      Pers doc - mat didactico");
+                                fileWrite.WriteLine(line.Substring(118, 15).PadRight(15) +"   " + line.Substring(118, 15).Length.ToString().PadRight(13) + "      Rem no alcanzada/ exenta s/ hs extras");
+                                fileWrite.WriteLine(line.Substring(133, 15).PadRight(15) +"   " + line.Substring(133, 15).Length.ToString().PadRight(13) + "      Rem exenta hs extras");
+                                fileWrite.WriteLine(line.Substring(148, 15).PadRight(15) +"   " + line.Substring(148, 15).Length.ToString().PadRight(13) + "      Mov y viaticos rem exenta");
+                                fileWrite.WriteLine(line.Substring(163, 15).PadRight(15) +"   " + line.Substring(163, 15).Length.ToString().PadRight(13) + "      Pers doc - mat didactico exento");
+                                fileWrite.WriteLine(line.Substring(178, 15).PadRight(15) +"   " + line.Substring(178, 15).Length.ToString().PadRight(13) + "      Rem otros emp- rem bruta");
+                                fileWrite.WriteLine(line.Substring(193, 15).PadRight(15) +"   " + line.Substring(193, 15).Length.ToString().PadRight(13) + "      Rem otros emp- rem bruta");
+                                fileWrite.WriteLine(line.Substring(208, 15).PadRight(15) +"   " + line.Substring(208, 15).Length.ToString().PadRight(13) + "      SAC 1 CUOTA otros emp");
+                                fileWrite.WriteLine(line.Substring(223, 15).PadRight(15) +"   " + line.Substring(223, 15).Length.ToString().PadRight(13) + "      SAC 2 CUOTA otros emp");
+                                fileWrite.WriteLine(line.Substring(238, 15).PadRight(15) +"   " + line.Substring(238, 15).Length.ToString().PadRight(13) + "      Hs extras grv - otros emp");
+                                fileWrite.WriteLine(line.Substring(253, 15).PadRight(15) +"   " + line.Substring(253, 15).Length.ToString().PadRight(13) + "      Mov y viaticos grv - otros emp");
+                                fileWrite.WriteLine(line.Substring(268, 15).PadRight(15) +"   " + line.Substring(268, 15).Length.ToString().PadRight(13) + "      Docente mat. didactico grv - otros emp");
+                                fileWrite.WriteLine(line.Substring(283, 15).PadRight(15) +"   " + line.Substring(283, 15).Length.ToString().PadRight(13) + "      No alcan/ex(sin hs extras)");
+                                fileWrite.WriteLine(line.Substring(298, 15).PadRight(15) +"   " + line.Substring(298, 15).Length.ToString().PadRight(13) + "      Exenta hs extras - otros empl");
+                                fileWrite.WriteLine(line.Substring(313, 15).PadRight(15) +"   " + line.Substring(313, 15).Length.ToString().PadRight(13) + "      Mov y viaticos exenta- Rem otros empl");
+                                fileWrite.WriteLine(line.Substring(328, 15).PadRight(15) +"   " + line.Substring(328, 15).Length.ToString().PadRight(13) + "      Doc mat y diactico exento otros emp");
+                                fileWrite.WriteLine(line.Substring(343, 15).PadRight(15) +"   " + line.Substring(343, 15).Length.ToString().PadRight(13) + "      Rem gravada");
+                                fileWrite.WriteLine(line.Substring(358, 15).PadRight(15) +"   " + line.Substring(358, 15).Length.ToString().PadRight(13) + "      Rem no grav/ no alcanzada / exenta");
+                                fileWrite.WriteLine(line.Substring(373, 17).PadRight(15) +"   " + line.Substring(373, 17).Length.ToString().PadRight(13) + "      Total remuneraciones" + "\n");
+                                fileWrite.WriteLine("");
+                                break;
+                            case "04":
+                                fileWrite.WriteLine("Valor Campo:   Tamaño Campo:     Descripcion campo:");
+                                fileWrite.WriteLine(line.Substring(0, 2).PadRight(15) +"   " + line.Substring(0, 2).Length.ToString().PadRight(13) + "      Tipo Registro");
+                                fileWrite.WriteLine(line.Substring(2, 11).PadRight(15) +"   " + line.Substring(2, 11).Length.ToString().PadRight(13) + "      CUIL");
+                                fileWrite.WriteLine(line.Substring(13, 15).PadRight(15) +"   " + line.Substring(13, 15).Length.ToString().PadRight(13) + "      Ap. jub, retiro, pensions o subsidios");
+                                fileWrite.WriteLine(line.Substring(28, 15).PadRight(15) +"   " + line.Substring(28, 15).Length.ToString().PadRight(13) + "      Ot. emp.- Ap jub retiro, pensiones o subsidios");
+                                fileWrite.WriteLine(line.Substring(43, 15).PadRight(15) +"   " + line.Substring(43, 15).Length.ToString().PadRight(13) + "      Ap. obra social");
+                                fileWrite.WriteLine(line.Substring(58, 15).PadRight(15) +"   " + line.Substring(58, 15).Length.ToString().PadRight(13) + "      Ap. obra social otros emp");
+                                fileWrite.WriteLine(line.Substring(73, 15).PadRight(15) +"   " + line.Substring(73, 15).Length.ToString().PadRight(13) + "      Cuota sindical");
+                                fileWrite.WriteLine(line.Substring(88, 15).PadRight(15) +"   " + line.Substring(88, 15).Length.ToString().PadRight(13) + "      Cuota sindical otros emp");
+                                fileWrite.WriteLine(line.Substring(103, 15).PadRight(15) +"   " + line.Substring(103, 15).Length.ToString().PadRight(13) + "      Cuota medico asistencial");
+                                fileWrite.WriteLine(line.Substring(118, 15).PadRight(15) +"   " + line.Substring(118, 15).Length.ToString().PadRight(13) + "      Prismas de seg para el caso de muerte");
+                                fileWrite.WriteLine(line.Substring(133, 15).PadRight(15) +"   " + line.Substring(133, 15).Length.ToString().PadRight(13) + "      Seguro muerte/ mixtos sujetas al control SSN");
+                                fileWrite.WriteLine(line.Substring(148, 15).PadRight(15) +"   " + line.Substring(148, 15).Length.ToString().PadRight(13) + "      Seguro de retiro priv - sujetas  SNN");
+                                fileWrite.WriteLine(line.Substring(163, 15).PadRight(15) +"   " + line.Substring(163, 15).Length.ToString().PadRight(13) + "      Adq de cuotaparte FCI");
+                                fileWrite.WriteLine(line.Substring(178, 15).PadRight(15) +"   " + line.Substring(178, 15).Length.ToString().PadRight(13) + "      Gastos de sepelio");
+                                fileWrite.WriteLine(line.Substring(193, 15).PadRight(15) +"   " + line.Substring(193, 15).Length.ToString().PadRight(13) + "      Gtos. amort e int rodado corredores y viaj de comercio");
+                                fileWrite.WriteLine(line.Substring(208, 15).PadRight(15) +"   " + line.Substring(208, 15).Length.ToString().PadRight(13) + "      Donc fiscos Nac/ prov / mun");
+                                fileWrite.WriteLine(line.Substring(223, 15).PadRight(15) +"   " + line.Substring(223, 15).Length.ToString().PadRight(13) + "      Dtos oblig por ley nac / prov / munc");
+                                fileWrite.WriteLine(line.Substring(238, 15).PadRight(15) +"   " + line.Substring(238, 15).Length.ToString().PadRight(13) + "      Honorarios serv asistencial sant medica y panamerica");
+                                fileWrite.WriteLine(line.Substring(253, 15).PadRight(15) +"   " + line.Substring(253, 15).Length.ToString().PadRight(13) + "      Intereses cerd hipotecarios");
+                                fileWrite.WriteLine(line.Substring(268, 15).PadRight(15) +"   " + line.Substring(268, 15).Length.ToString().PadRight(13) + "      AP. cap soc / fondos de riesgo soc sgr");
+                                fileWrite.WriteLine(line.Substring(283, 15).PadRight(15) +"   " + line.Substring(283, 15).Length.ToString().PadRight(13) + "      Ap. cajas compl/ fondos compensadores de prev/ similares");
+                                fileWrite.WriteLine(line.Substring(298, 15).PadRight(15) +"   " + line.Substring(298, 15).Length.ToString().PadRight(13) + "      Alq inmuebles destinos a su casa hab");
+                                fileWrite.WriteLine(line.Substring(313, 15).PadRight(15) +"   " + line.Substring(313, 15).Length.ToString().PadRight(13) + "      empleado serv domestico");
+                                fileWrite.WriteLine(line.Substring(328, 15).PadRight(15) +"   " + line.Substring(328, 15).Length.ToString().PadRight(13) + "      Gstos mov/viaticos abonados por el empleador");
+                                fileWrite.WriteLine(line.Substring(343, 15).PadRight(15) +"   " + line.Substring(343, 15).Length.ToString().PadRight(13) + "      Indumentaria / equipamiento- uso exlusivo caracter obligatorio");
+                                fileWrite.WriteLine(line.Substring(358, 15).PadRight(15) +"   " + line.Substring(358, 15).Length.ToString().PadRight(13) + "      Otras deduccionnes");
+                                fileWrite.WriteLine(line.Substring(373, 17).PadRight(15) +"   " + line.Substring(373, 17).Length.ToString().PadRight(13) + "      Total remuneraciones Generales" + "\n");
+                                fileWrite.WriteLine("");
+                                break;
+                            case "05":
+                                fileWrite.WriteLine("Valor Campo:   Tamaño Campo:     Descripcion campo:");
+                                fileWrite.WriteLine(line.Substring(2, 11).PadRight(15) + "CUIL LEGAJO       " + " Tamaño linea " + line.Length.ToString().PadRight(13) + "\n");
+                                fileWrite.WriteLine(line.Substring(0, 2).PadRight(15) +"   " + line.Substring(0, 2).Length.ToString().PadRight(13) + "      Tipo Registro");
+                                fileWrite.WriteLine(line.Substring(2, 11).PadRight(15) +"   " + line.Substring(2, 11).Length.ToString().PadRight(13) + "      CUIL");
+                                fileWrite.WriteLine(line.Substring(13, 15).PadRight(15) +"   " + line.Substring(13, 15).Length.ToString().PadRight(13) + "      Alicuota art ");
+                                fileWrite.WriteLine(line.Substring(28, 15).PadRight(15) +"   " + line.Substring(28, 15).Length.ToString().PadRight(13) + "      Deduccion especial");
+                                fileWrite.WriteLine(line.Substring(43, 15).PadRight(15) +"   " + line.Substring(43, 15).Length.ToString().PadRight(13) + "        Deduccion epecifica");
+                                fileWrite.WriteLine(line.Substring(58, 15).PadRight(15) +"   " + line.Substring(58, 15).Length.ToString().PadRight(13) + "      Conyugue");
+                                fileWrite.WriteLine(line.Substring(73, 2).PadRight(15) +"   " + line.Substring(73, 2).Length.ToString().PadRight(13) + "      cant. hijos");
+                                fileWrite.WriteLine(line.Substring(75, 15).PadRight(15) +"   " + line.Substring(75, 15).Length.ToString().PadRight(13) + "      Hijos/ hijastros");
+                                fileWrite.WriteLine(line.Substring(90, 15).PadRight(15) +"   " + line.Substring(90, 15).Length.ToString().PadRight(13) + "      Total de cargas flia");
+                                fileWrite.WriteLine(line.Substring(105, 15).PadRight(15) +"   " + line.Substring(105, 15).Length.ToString().PadRight(13) + "      Total deducciones art. 23");
+                                fileWrite.WriteLine(line.Substring(120, 15).PadRight(15) +"   " + line.Substring(120, 15).Length.ToString().PadRight(13) + "      Rem suj a impuestos" + "\n");
+                                fileWrite.WriteLine("");
+                                break;
+                            case "06":
+                                fileWrite.WriteLine("Valor Campo:   Tamaño Campo:     Descripcion campo:");
+                                fileWrite.WriteLine(line.Substring(0, 2).PadRight(15) +"   " + line.Substring(0, 2).Length.ToString().PadRight(13) + "      Tipo Registro");
+                                fileWrite.WriteLine(line.Substring(2, 11).PadRight(15) +"   " + line.Substring(2, 11).Length.ToString().PadRight(13) + "      CUIL");
+                                fileWrite.WriteLine(line.Substring(13, 1).PadRight(15) +"   " + line.Substring(13, 1).Length.ToString().PadRight(13) + "      Alicuota art 90 ley ganancia");
+                                fileWrite.WriteLine(line.Substring(14, 1).PadRight(15) +"   " + line.Substring(14, 1).Length.ToString().PadRight(13) + "      Alicuota aplicable( sin inc hs extras)");
+                                fileWrite.WriteLine(line.Substring(15, 15).PadRight(15) +"   " + line.Substring(15, 15).Length.ToString().PadRight(13) + "      Impuestos determinado");
+                                fileWrite.WriteLine(line.Substring(30, 15).PadRight(15) +"   " + line.Substring(30, 15).Length.ToString().PadRight(13) + "      Impuesto retenido");
+                                fileWrite.WriteLine(line.Substring(45, 15).PadRight(15) +"   " + line.Substring(45, 15).Length.ToString().PadRight(13) + "      Pagos a cuenta");
+                                fileWrite.WriteLine(line.Substring(60, 15).PadRight(15) +"   " + line.Substring(60, 15).Length.ToString().PadRight(13) + "      Saldo" + "\n");
+                                fileWrite.WriteLine("");
+                                break;
+                        }
+
+
+                    }
+                }
+
+
+
+
+                ////aqui se renombrea el archivo temporal
+                //File.Delete(@"C:\carpeta\F1357.11111111111.20180000.0001.txt");
+                //File.Move(@"c:\carpeta\temp.txt", @"c:\carpeta\F1357.11111111111.20180000.0001.txt");
+            }
+        }
+    }
+}
